3a9f18021b3972d70ac6bc375cbfb28fbf252400
AndrikSokol <AndrikSokol31@gmail.com>
2023-03-10 02:47:08 +0300

    created route | changed css

--- src/App.js
+++ src/App.js
@@ -1,94 +1,23 @@
+import { BrowserRouter, Link, Route,Routes } from 'react-router-dom';
 import './App.scss';
-import React from 'react';
-import PostList from './components/PostList';
-import PostFilter from './components/PostFilter';
-import PostForm from './components/PostForm';
-import MyModal from './components/UI/myModal/MyModal';
-import MyButton from './components/UI/button/MyButton';
-import { usePosts } from './hooks/usePosts';
-import PostService from './API/PostService';
-import { Loader } from './components/UI/Loader/Loader';
-import { useFetching } from './hooks/useFetching';
-import { getPageCount } from './utils/pages';
-import { usePaginations } from './hooks/usePaginations';
-import SwitchPages from './components/SwitchPages';
-
+import About from './pages/About';
+import Posts from './pages/Posts'
+import Error from './pages/Error'
 
 const App = () => {
 
- 
-  const [posts,setPosts] = React.useState([]);
-  const [filter,setFilter] = React.useState({sort:'',query:''})
-  const [modal,setModal] = React.useState('')
-  const sortedAndSearchedPosts = usePosts(posts,filter.sort,filter.query);
-  const[totalPages,setTotalPages] = React.useState(0)
-  const[page,setPage] = React.useState(1)
-  const[limit,setLimit] = React.useState(10)
-
-  const[fetchPosts,isPostsLoading,postError]  = useFetching(async ()=>{
-    const response = await PostService.getAll(page,limit);
-    setPosts(response.data)
-    console.log("work")
-    const totalCount  = await response.headers['x-total-count']
-    setTotalPages(getPageCount(totalCount,limit))
-  })
-   
-  const pagesArray = [];
-    for (let i = 0; i < totalPages; i++) {
-      pagesArray.push(i+1);
-    }
-
-  React.useEffect( ()=>{
-    fetchPosts()
-    
-  }
-  ,[page])
-
-  // const pagesArray = usePaginations(totalPages);
-  
-  const createPost = (newPost) =>{
-    setPosts(prev => [...prev, newPost]);
-    setModal(false)
-  }
-
-  const removePost =(post)=>{
-    setPosts(posts.filter(obj=> obj.id !== post.id))
-  }
-
-  const changePage =(page)=>{
-    setPage(page)
-  }
-
-  return (  
-    <>
-    <header></header>
+  return (
     <div className='App'>
-      <MyButton onClick ={()=>setModal(true)}>Создать пост</MyButton>
-      <MyModal visible = {modal} setVisible ={setModal}>
-        <PostForm  create = {createPost}/>
-      </MyModal>
-         
-      <PostFilter 
-        filter = {filter} 
-        setFilter = {setFilter}
-      />
-      {postError &&
-        <h1>Ошибка загрузки ${postError}</h1>
-      }
-      {isPostsLoading
-        ? (
-          <div style={{display:'flex',justifyContent: 'center'}}><Loader/></div>
-        )
-        : (
-          <>
-          <SwitchPages totalPages={totalPages} pagesArray ={pagesArray} changePage ={changePage} page = {page}/>,
-          <PostList posts = {sortedAndSearchedPosts} title = 'Посты' removePost = {removePost}/>,
-          <SwitchPages totalPages={totalPages} pagesArray ={pagesArray} changePage ={changePage} page = {page}/>
-          </>
-         ) 
-      }
-      </div>
-    </>
+      <header>
+        <Link to ='/about'>О Сайте</Link>
+        <Link to='/posts'>Посты</Link>
+      </header> 
+      <Routes>
+        <Route path="about" element={ <About/>}/>
+        <Route path="posts" element={ <Posts/>}/>
+        <Route path="*" element={ <Error/>}/>
+      </Routes>
+    </div>
   );
 }
 
--- src/components/PostItem.jsx
+++ src/components/PostItem.jsx
@@ -1,14 +1,15 @@
 import React from 'react'
 import MyButton from './UI/button/MyButton';
 import style from './PostItem.module.scss'
-
+import pushPin from '../assets/red-pushpin.png'
 const PostItem = ({post,removePost,number}) =>{
 
     return (
         <div className={style.PostItem}>
           <div>
+            <img style={{width:'30px',objectFit:'cover'}}src ={pushPin} alt ='Красная канцелярская кнопка'></img>
             <h3>{post.id}. {post.title}</h3>
-            <p>{post.text}</p>
+            <p>{post.body}</p>
           </div>
           <div>
             <MyButton onClick = {() =>removePost(post)} >Удалить</MyButton>
